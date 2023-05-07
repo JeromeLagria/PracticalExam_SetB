2f2c15e09b9abedeb4dcf44fac66cf01c215811c
JeromeLagria <lagria.jerome1@gmail.com>
2023-05-07 18:25:18 +0800

    Final Commit

---
+++ app/Models/User.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace App\Models;
+    use Illuminate\Database\Eloquent\Model;
+
+    class User extends Model{
+        protected $table = 'tblteacher';
+
+        protected $fillable = [
+
+            'teacherid','lastname','firstname', 'middlename', 'bday', 'age'
+        ];
+
+        public $timestamps = false;
+        
+        protected $primaryKey = 'teacherid';
+    }
+
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('/users',['uses' => 'TeacherController@getUsers']);
+});
+
+$router->get('/users',['uses' => 'TeacherController@getUsers']);
+$router->post('/users',['uses' => 'TeacherController@add']);
+$router->put('/users/{id}', 'TeacherController@update');
+$router->delete('/users/{id}',['uses' => 'TeacherController@deleteTeacher']);
+$router->get('/users/{id}', 'TeacherController@show');
